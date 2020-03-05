70c57f7990f0471d050ce187e9bf1cdabd5ceee4
Sam White <Sam.White745@gmail.com>
2020-03-05 16:23:55 +0000

    Beers and Exercises

--- src/main/scala/day4/day4.scala
+++ src/main/scala/day4/day4.scala
@@ -21,7 +21,34 @@ object day4 extends App {
     cardNumber*10 + (10 - luhn(cardNumber, true))
   }
 
-  println(s"is 49927398716 a valid credit card number: ${validCreditCard(49927398716L)}")
-  println(s"without check num: 4992739871, with check num: ${addCheckDigit(4992739871L)}")
+//  println(s"is 49927398716 a valid credit card number: ${validCreditCard(49927398716L)}")
+//  println(s"without check num: 4992739871, with check num: ${addCheckDigit(4992739871L)}")
+
+  def blackJack(card1:Int, card2:Int) = (card1, card2) match{
+    case (0, y) if y<=21 => y
+    case (x, 0) if x<=21 => x
+    case (x,y) if x>21 && y <= 21 => y
+    case (x,y) if x<=21 && y > 21 => x
+    case (x,y) => if (x > y) x else y
+  }
+
+//  println(blackJack(22,18))
+
+  def uniqueSum(num1:Int, num2:Int, num3:Int): Int = {
+    var total = 0
+    if (num1 != num2 && num1 != num3) total+= num1
+    if (num2 != num1 && num2 != num3) total+= num2
+    if (num3 != num1 && num3 != num2) total+= num3
+    total
+  }
+//  println(uniqueSum(1,4,4))
+//  println(uniqueSum(3,3,3))
+
+  def tooHot(temperature:Int, isSummer:Boolean): Boolean = {
+    if (isSummer) (temperature >= 60 && temperature <=100)
+    else (temperature >=60 && temperature <= 90)
+  }
+
+
 
 }
